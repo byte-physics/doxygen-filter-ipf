byte-physics/doxygen-filter-ipf

commit e3822b477536b068f12b3a54c17e926f5bcf50ce

Support inline parameter declarations in all cases
Until now inline parameter declarations were only supported when
multiple return value syntax was used.

This is now rectified. In addition some bugs were fixed in the inline
parameter parsing.

doxygen-filter-ipf.awk
@@ -93,6 +93,12 @@ function splitIntoWords(str, a)
   return split(str,a,/[[:space:],&*]+/)
 }
 
+# Split at comma
+function splitAtComma(str, a)
+{
+  return split(str,a,/,/)
+}
+
 # Split params into words and prefix each with "__Param__$i"
 # where $i is increased for every parameter
 # Returns the concatenation of all prefixed parameters
@@ -126,45 +132,88 @@ function handleParameterOldStyle(params, a,  i, iOpt, str, entry)
 # to handle optional parameters
 function handleParameterNewStyle(params, a, i, iOpt, str, entry)
 {
-  numParams = splitIntoWords(params, a)
+  numParams = splitAtComma(params, tokenArray)
 
   # print "numParams: " numParams
 
   str=""
-  iOpt=numParams
-  for(i=1; i <= numParams; i += 2)
+  iOpt=1e6
+  for(i=1; i <= numParams; i += 1)
   {
-    # print a[i]
-    # print a[i + 1]
+    tokenArray[i] = trim(tokenArray[i])
+    # print "tokenArray[i]: " tokenArray[i]
+
+    if(match(tokenArray[i],/\[$/))
+      iOpt = i + 1
+    else if(match(tokenArray[i],/\[[a-z]+/))
+      iOpt = i
+
+    gsub(/[\[\]]/, "", tokenArray[i])
 
     # convert igor optional parameters to something doxygen understands
     # igor dictates that the optional arguments are the last arguments,
     # meaning no normal argument can follow the optional arguments
-    if(gsub(/[\[\]]/,"",a[i]) || i > iOpt)
+    if(i >= iOpt)
     {
-      gsub(/[\[\]]/,"",a[i])
-      gsub(/[\[\]]/,"",a[i + 1])
-      iOpt  = i
-      entry = a[i] " " a[i + 1] " = defaultValue"
-    }
-    else if(gsub(/\[/,"",a[i + 1]))
-    {
-      gsub(/[\[\]]/,"",a[i + 1])
-      entry = a[i] " " a[i + 1]
-      iOpt  = i
+      entry = formatSingleNewStyleParameter(tokenArray[i], 1)
     }
     else
     {
-      entry = a[i] " " a[i + 1]
+      entry = formatSingleNewStyleParameter(tokenArray[i], 0)
     }
 
     str = str "" entry
 
-    if(i < numParams - 1)
+    if(i < numParams)
      str = str ", "
   }
 
   return str
+}
+
+# Format the given token as inline function parameter
+# isOptional is a boolean parameter
+function formatSingleNewStyleParameter(token, isOptional,  numElements, name, type, paramLine)
+{
+  if(match(token,/&/))
+  {
+    typeSuffix = "*"
+  }
+  else
+  {
+    typeSuffix = ""
+  }
+
+  # translate module separator "#" to C++ namespace separator
+  gsub("#", "::", token)
+
+  numElements = splitIntoWords(token, a)
+
+  # print "a[1]: " a[1]
+  # print "a[2]: " a[2]
+  # print "a[3]: " a[3]
+
+  if(numElements == 3 && (a[1] == "funcref" || a[1] == "struct"))
+  {
+    type = a[2]
+    name = a[3]
+  }
+  else
+  {
+    type = a[1]
+    name = a[2]
+  }
+
+  type = tolower(type)
+
+  paramLine = nicifyWaveType(type) typeSuffix " " name
+
+  if(isOptional)
+  {
+    paramLine = paramLine " = defaultValue"
+  }
+
+  return paramLine
 }
 
 {
@@ -248,13 +297,15 @@ function handleParameterNewStyle(params, a, i, iOpt, str, entry)
 
     if(match(code,/\(.*[a-z]+.*\)/))
     {
-      paramStr = substr(code,RSTART+1,RLENGTH-2)
-
-      # print "optional parameter: " paramStr
-
+      paramsStartIndex = RSTART
+      paramsLength = RLENGTH
+      paramStr = substr(code, paramsStartIndex + 1, paramsLength - 2)
       paramStrWithTypes = handleParameterNewStyle(paramStr, params)
 
-      code = substr(code,1,RSTART) "" paramStrWithTypes "" substr(code,RSTART+RLENGTH-1)
+      # print "paramStr __ " paramStr
+      # print "paramStrWithTypes __ " paramStrWithTypes
+
+      code = substr(code, 1, paramsStartIndex) "" paramStrWithTypes "" substr(code, paramsStartIndex + paramsLength - 1)
     }
   }
   else if(!insideFunction && match(code,/^((threadsafe|static|override)?[[:space:]]+)*function[[:space:]]*(\/(df|wave|c|s|t|d))?[[:space:]]+[A-Z0-9_]+[[:space:]]*\(/))
@@ -280,15 +331,28 @@ function handleParameterNewStyle(params, a, i, iOpt, str, entry)
     # do we have function parameters
     if(match(code,/\(.*[a-z]+.*\)/))
     {
-      paramStr = substr(code,RSTART+1,RLENGTH-2)
+      paramsStartIndex = RSTART
+      paramsLength = RLENGTH
+      paramStr = substr(code, paramsStartIndex + 1,paramsLength - 2)
 
-      paramStrWithTypes = handleParameterOldStyle(paramStr, params)
-      paramsToHandle = numParams
+      if(match(paramStr, /^(variable|string|wave|dfref|funcref|struct|int|int64|uint64|double|complex)\y/))
+      {
+        # inline parameter declaration
+        paramStrWithTypes = handleParameterNewStyle(paramStr, params)
+      }
+      else
+      {
+        # old style parameter declaration in code in the function
+
+        paramStrWithTypes = handleParameterOldStyle(paramStr, params)
+        paramsToHandle = numParams
+
+        # print "paramsToHandle __ " paramsToHandle
+      }
+
       # print "paramStr __ " paramStr
       # print "paramStrWithTypes __ " paramStrWithTypes
-      # print "paramsToHandle __ " paramsToHandle
-
-      code = substr(code,1,RSTART) "" paramStrWithTypes "" substr(code,RSTART+RLENGTH-1)
+      code = substr(code, 1, paramsStartIndex) "" paramStrWithTypes "" substr(code, paramsStartIndex + paramsLength - 1)
     }
   }
   else if(insideFunction && paramsToHandle > 0)

test-input-function-params.ipf
@@ -78,6 +78,22 @@ End
 // and this is not a function declaration
 SomeStuff Function notAFunc()
 
-// TODO currently broken
-static Function myFuncBroken(variable var)
+Function Proto()
+
+End
+
+Function Test1(variable a, string s, wave w, dfref d, funcref Proto f, struct RECTF &st, int i, int64 i64, uint64 ui64, double do, complex c, [variable opt])
+
+End
+
+Function Test2(variable &a, funcref Proto f, [string &s])
+
+End
+
+Function Test3(struct RECTF& s [, string s])
+
+End
+
+Function/S TestWithNastyPrefix(waverefs)
+	Wave/WAVE waveRefs
 End

test-input-function-params.output.ref
@@ -78,7 +78,22 @@ static variable myFuncS2(variable var){
 // and this is not a function declaration
 SomeStuff Function notAFunc();
 
-// TODO currently broken
-static variable myFuncBroken(__Param__1 variable, __Param__2 var){
-End
+variable Proto(){
+
+};
+
+variable Test1(variable a, string s, wave w, dfref d, proto f, rectf* st, int i, int64 i64, uint64 ui64, double do, complex c, variable opt = defaultValue){
+
+};
+
+variable Test2(variable* a, proto f, string* s = defaultValue){
+
+};
+variable Test3(struct* RECTF, string s = defaultValue){
+
+};
+
+string TestWithNastyPrefix(WaveRefWave waverefs){
+	WaveRefWave waveRefs
+};
 

commit ec2b469473564897655120a71e77f51b827dbadc

Fix parameter casing for inline parameters
We want to lower case the type only for types which are not struct or
funcref, as otherwise the name is lowercased and that breaks doxygen
linking.

Bug introduced in e3822b47 (Support inline parameter declarations in all
cases, 2020-04-03).

doxygen-filter-ipf.awk
@@ -200,11 +200,9 @@ function formatSingleNewStyleParameter(token, isOptional,  numElements, name, ty
   }
   else
   {
-    type = a[1]
+    type = tolower(a[1])
     name = a[2]
   }
-
-  type = tolower(type)
 
   paramLine = nicifyWaveType(type) typeSuffix " " name
 

test-input-function-params.ipf
@@ -101,3 +101,15 @@ End
 Function/S TestWithNastyPrefix(waverefs)
 	Wave/WAVE waveRefs
 End
+
+Structure MyStruct
+	int32 count
+EndStructure
+
+Function PA_ApplyPulseSortingOrder(WAVE setIndices, STRUCT MyStruct &myStruct)
+End
+
+Function PA_GatherSettings(win, myStruct)
+	string win
+	STRUCT MyStruct &myStruct
+End

test-input-function-params.output.ref
@@ -86,11 +86,11 @@ variable Proto(){
 
 };
 
-variable Test1(variable a, string s, wave w, dfref d, proto f, rectf* st, int i, int64 i64, uint64 ui64, double do, complex c, variable opt = defaultValue){
+variable Test1(variable a, string s, wave w, dfref d, Proto f, RECTF* st, int i, int64 i64, uint64 ui64, double do, complex c, variable opt = defaultValue){
 
 };
 
-variable Test2(variable* a, proto f, string* s = defaultValue){
+variable Test2(variable* a, Proto f, string* s = defaultValue){
 
 };
 
@@ -100,5 +100,17 @@ variable Test3(struct* RECTF, string s = defaultValue){
 
 string TestWithNastyPrefix(WaveRefWave waverefs){
 	WaveRefWave waveRefs
+};
+
+struct MyStruct{;
+	int32 count;
+};
+
+variable PA_ApplyPulseSortingOrder(wave setIndices, MyStruct* myStruct){
+};
+
+variable PA_GatherSettings(string win, MyStruct* myStruct){
+	string win
+	STRUCT MyStruct &myStruct
 };
 